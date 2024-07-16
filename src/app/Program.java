ba03a52cff9661f5ec319de5cc0681c6f0f77a29
Edson Lima <111332935+edsuuu@users.noreply.github.com>
2024-07-16 00:51:53 -0300

    Initial commit

---
+++ src/app/Program.java
@@ -0,0 +1,16 @@
+package app;
+
+public class Program {
+    public static void main(String[] args) {
+        int a, b;
+        double resultado;
+
+        a = 5;
+        b = 2;
+        resultado = (double) a / b;
+
+        System.out.println(resultado);
+
+
+    }
+}
---
+++ src/entities/Product.java
@@ -0,0 +1,13 @@
+package entities;
+
+public class Product {
+    private String name;
+    private Double price;
+
+    public Product(String name, Double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    // alt e insert para abrir a opcao de construtor/ getters/ setters
+}
